579cd6fc71a04009310d51178c4346fd4ec1e8c0
Александр Луценко <a.loutsenko@team.superjob.ru>
2023-09-07 19:45:34 +0300

    Add unit-test for gz-encoded sentry event

---
+++ tests/App/Modules/Sentry/Interfaces/Http/Handler/EventHandlerTest.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\App\Modules\Sentry\Interfaces\Http\Handler;
+
+use App\Application\HTTP\GzippedStreamFactory;
+use Modules\Sentry\Application\EventHandlerInterface;
+use Modules\Sentry\Interfaces\Http\Handler\EventHandler;
+use Nyholm\Psr7\Response;
+use Nyholm\Psr7\ServerRequest;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Spiral\Cqrs\CommandBusInterface;
+use Spiral\Files\FilesInterface;
+use Spiral\Http\ResponseWrapper;
+
+class EventHandlerTest extends TestCase
+{
+    public function testHandleGzippedEventData()
+    {
+        $factory = new GzippedStreamFactory;
+        $eventHandler = $this->createMock(EventHandlerInterface::class);
+        $commandBus = $this->createMock(CommandBusInterface::class);
+
+        $handler = new EventHandler(
+            $factory,
+            new ResponseWrapper(
+                $this->createMock(ResponseFactoryInterface::class),
+                $this->createMock(StreamFactoryInterface::class),
+                $this->createMock(FilesInterface::class),
+            ),
+            $eventHandler,
+            $commandBus,
+        );
+        $body = '{"event_id": "81d68250-ebb4-461a-935f-1d9df8309f7f", "level": "Error", "platform": "other", "environment": "None"}';
+        $body = gzencode($body);
+        $serverRequest = new ServerRequest('POST', 'http://sentry@127.0.0.1:3001/1/store', [
+            'Content-Encoding' => 'gzip'
+        ], $body);
+
+
+        $commandBus->expects($this->once())->method('dispatch');
+        $handler->handle($serverRequest, fn(ServerRequestInterface $request) => new Response());
+    }
+
+    public function testHandlePLaintextEventData()
+    {
+        $factory = new GzippedStreamFactory;
+        $eventHandler = $this->createMock(EventHandlerInterface::class);
+        $commandBus = $this->createMock(CommandBusInterface::class);
+
+        $handler = new EventHandler(
+            $factory,
+            new ResponseWrapper(
+                $this->createMock(ResponseFactoryInterface::class),
+                $this->createMock(StreamFactoryInterface::class),
+                $this->createMock(FilesInterface::class),
+            ),
+            $eventHandler,
+            $commandBus,
+        );
+        $body = '{"event_id": "81d68250-ebb4-461a-935f-1d9df8309f7f", "level": "Error", "platform": "other", "environment": "None"}';
+        $serverRequest = new ServerRequest('POST', 'http://sentry@127.0.0.1:3001/1/store', [], $body);
+
+
+        $commandBus->expects($this->once())->method('dispatch');
+        $handler->handle($serverRequest, fn(ServerRequestInterface $request) => new Response());
+    }
+}
